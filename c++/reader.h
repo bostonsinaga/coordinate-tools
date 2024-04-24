08dafae182ff45b3802f40017cd48d9c176467eb
bostonsinaga <bostonsinga@gmail.com>
2024-04-24 21:05:02 +0700

    Set declaration inside namespace

--- c++/reader.h
+++ c++/reader.h
@@ -1,14 +1,37 @@
-namespace Reader {
-    std::string testDecimal(std::string text) {
-        std::string latStr, lngStr;
+#ifndef __COORDINATE_TOOLS__READER_H__
+#define __COORDINATE_TOOLS__READER_H__
 
-        for (int i = 0; i < text.length(); i++) {
-            if (text[i]) {
-                
-            }
-        }
+#include <ctype.h>
+#include <vector>
+#include <string>
+#include "point.h"
+
+namespace coordinate_tools {
+  class Reader {
+  private:
+    std::vector<Point> points;
+
+  public:
+    Reader() {}
+
+    // ex: [ -7.123, 110.123 ]
+    bool testDecimal(std::string text);
+
+    // ex: [ 7°7'22.80"S, 110°7'22.80"E ]
+    bool testDMS() {
+      return true;
     }
 
-    std::string testDMS();
+    // returns the first vector
+    Point getSinglePoint() {
+      if (points.size() > 0) {
+        return points.front();
+      }
+      else return Point();
+    }
+
+    std::vector<Point> getAllPoints() { return points; }
+  };
 }
 
+#endif // __COORDINATE_TOOLS__READER_H__
