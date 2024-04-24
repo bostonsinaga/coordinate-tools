adf43b3beb65702d4f75c3caaaa775464d1bd258
bostonsinaga <bostonsinga@gmail.com>
2024-04-24 21:12:24 +0700

    Test decimal reader

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "coordinate-tools.h"
 
-int main() {
-    
+using namespace coordinate_tools;
+
+int main(int argc, char *argv[]) {    
+    Reader reader = Reader();
+
+    if (reader.testDecimal(argv[1])) {
+        for (auto &pt : reader.getAllPoints()) {
+            std::cout << "Lat: " << pt.lat << ", ";
+            std::cout << "Lng: " << pt.lng << std::endl;
+        }
+    }
+
     return 0;
 }
 
