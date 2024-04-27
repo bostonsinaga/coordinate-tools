a2aa02b1c9a2b32281b3da4dd9f1d06a5431fd39
bostonsinaga <bostonsinga@gmail.com>
2024-04-27 10:24:47 +0700

    Using 'Parser' and its 'reset' method

--- c++/main.cpp
+++ c++/main.cpp
@@ -4,10 +4,12 @@
 using namespace coordinate_tools;
 
 int main(int argc, char *argv[]) {    
-    Parser reader = Parser();
 
-    if (reader.testDecimal(argv[1])) {
-        for (auto &pt : reader.getAllPoints()) {
+    Parser parser;
+    parser.reset();
+
+    if (parser.testDecimal(argv[1])) {
+        for (auto &pt : parser.getAllPoints()) {
             std::cout << "Lat: " << pt.lat << ", ";
             std::cout << "Lng: " << pt.lng << std::endl;
         }
