53c717371fdf959ae1e9be391bb9be4d9d2cc474
bostonsinaga <bostonsinga@gmail.com>
2024-05-03 16:33:04 +0700

    Differentiate 'DecimalPoint' and 'DMSPoint'

--- c++/point.h
+++ c++/point.h
@@ -1,27 +1,117 @@
 #ifndef __COORDINATE_TOOLS__POINT_H__
 #define __COORDINATE_TOOLS__POINT_H__
 
+#include <iomanip>
+#include <sstream>
+#include "definitions.h"
+
 namespace coordinate_tools {
-  class Point {
+  class DecimalPoint {
   public:
-    Point() {}
+    DecimalPoint() {}
 
-    Point(Point &pt_in) {
+    DecimalPoint(DecimalPoint &pt_in) {
       lat = pt_in.lat;
       lng = pt_in.lng;
     }
 
-    Point(const Point &pt_in) {
+    DecimalPoint(const DecimalPoint &pt_in) {
       lat = pt_in.lat;
       lng = pt_in.lng;
     }
 
-    Point(double lat_in, double lng_in) {
+    DecimalPoint(double lat_in, double lng_in) {
       lat = lat_in;
       lng = lng_in;
     }
 
     double lat = 0, lng = 0;
+  };
+
+  class DMSPoint {
+  public:
+    struct Axis {
+      int deg = 0, min = 0;
+      double sec = 0.0;
+    } lat, lng;
+
+  private:
+    std::string getLetter(Axis *axis, int &axisPart) {
+      // positive
+      if (axis->deg >= 0) {
+        if (axisPart == LAT_PART) return "N";
+        else if (axisPart == LNG_PART) return "E";
+      }
+      // negative
+      else {
+        if (axisPart == LAT_PART) return "S";
+        else if (axisPart == LNG_PART) return "W";
+      }
+      return "";
+    }
+
+  public:
+    DMSPoint() {}
+
+    DMSPoint(DMSPoint &pt_in) {
+      lat = pt_in.lat;
+      lng = pt_in.lng;
+    }
+
+    DMSPoint(const DMSPoint &pt_in) {
+      lat = pt_in.lat;
+      lng = pt_in.lng;
+    }
+
+    DMSPoint(
+      int lat_deg, int lat_min, double lat_sec,
+      int lng_deg, int lng_min, double lng_sec
+    ) {
+      lat.deg = lat_deg;
+      lat.min = lat_min;
+      lat.sec = lat_sec;
+      lng.deg = lng_deg;
+      lng.min = lng_min;
+      lng.sec = lng_sec;
+    }
+
+    void empty() {
+      lat.deg = 0;
+      lat.min = 0;
+      lat.sec = 0;
+      lng.deg = 0;
+      lng.min = 0;
+      lng.sec = 0;
+    }
+
+    std::string stringifySingle(int axisPart) {
+      Axis *usedPart;
+
+      if (axisPart == LAT_PART) usedPart = &lat;
+      else if (axisPart == LNG_PART) usedPart = &lng;
+      else return "";
+
+      std::stringstream secStr;
+      secStr << std::fixed << std::setprecision(2) << usedPart->sec;
+
+      return (
+        std::to_string(std::abs(usedPart->deg)) + "\370" + 
+        std::to_string(usedPart->min) + "'" +
+        secStr.str() + "\"" +
+        getLetter(usedPart, axisPart)
+      );
+    }
+
+    std::string stringifyPair(std::string separator = ",", bool swapped = false) {
+      if (swapped) return (
+        stringifySingle(LNG_PART) + separator +
+        stringifySingle(LAT_PART)
+      );
+      else return (
+        stringifySingle(LAT_PART) + separator +
+        stringifySingle(LNG_PART)
+      );
+    }
   };
 }
 
