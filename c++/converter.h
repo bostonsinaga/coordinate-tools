875a35cf4f7f26ec4854d9e89dfb84f64bd290b2
bostonsinaga <bostonsinga@gmail.com>
2024-05-11 01:09:28 +0700

    Set normalize and switch value methods

--- c++/converter.h
+++ c++/converter.h
@@ -1,6 +1,17 @@
 #ifndef __COORDINATE_TOOLS__CONVERTER_H__
 #define __COORDINATE_TOOLS__CONVERTER_H__
 
+#include <vector>
+#include "point.h"
 
+namespace coordinate_tools {
+  class Converter {
+  public:
+    enum {MAX_DEG_90, MAX_DEG_180};
+    static void normalizeDecimalAngle(double &axis, int maxDegreeFlag = MAX_DEG_90);
+    static void switchDecimalAxis(DecimalPoint &pt);
+    static void switchDMSAxis(DMSPoint &pt);
+  };
+}
 
 #endif // __COORDINATE_TOOLS__CONVERTER_H__
