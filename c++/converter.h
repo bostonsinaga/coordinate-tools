d07b5879c12d6ff7b819059e79897ecfb919522e
bostonsinaga <bostonsinga@gmail.com>
2024-05-12 09:48:31 +0700

    Add 'setMaxAbsoluteDegree' and 'setQuadrantDeterminators' method

--- c++/converter.h
+++ c++/converter.h
@@ -6,9 +6,26 @@
 
 namespace coordinate_tools {
   class Converter {
+  private:
+    enum {qr_signs, qr_adjustments};
+
+    static void setMaxAbsoluteDegree(
+      int &maxAbsDegree,
+      int &maxDegreeFlag
+    );
+
+    static void setQuadrantDeterminators(
+      std::vector<int> &qrSigns,
+      std::vector<int> &qrAdjustments,
+      int valSign, int maxDegreeFlag, int maxAbsDegree
+    );
+
   public:
     enum {MAX_DEG_90, MAX_DEG_180};
+
     static void normalizeDecimalAngle(double &axis, int maxDegreeFlag = MAX_DEG_90);
+    static void normalizeDMSAngle(DMSAxis &axis, int maxDegreeFlag = MAX_DEG_90);
+
     static void switchDecimalAxis(DecimalPoint &pt);
     static void switchDMSAxis(DMSPoint &pt);
   };
