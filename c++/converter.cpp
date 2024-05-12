af49293da32b734d2548245b1075911a18ceaa32
bostonsinaga <bostonsinga@gmail.com>
2024-05-12 09:53:19 +0700

    Define 'normalizeDMSAngle', 'setMaxAbsoluteDegree' and 'setQuadrantDeterminators' methods

--- c++/converter.cpp
+++ c++/converter.cpp
@@ -5,7 +5,45 @@
 
 namespace coordinate_tools {
 
-  // ex: [ -7.123, 185.123 ] to [ -7.123, xxx.yyy ]
+  void Converter::setMaxAbsoluteDegree(
+    int &maxAbsDegree,
+    int &maxDegreeFlag
+  ) {
+    // set 'maxDegreeFlag' default to 'MAX_DEG_90'
+    if (maxDegreeFlag != MAX_DEG_90 && maxDegreeFlag != MAX_DEG_180) {
+      maxDegreeFlag = MAX_DEG_90;
+    }
+
+    // determine the degree limit
+    if (maxDegreeFlag == MAX_DEG_90) maxAbsDegree = 90;
+    else maxAbsDegree = 180;
+  }
+
+  void Converter::setQuadrantDeterminators(
+    std::vector<int> &qrSigns,
+    std::vector<int> &qrAdjustments,
+    int valSign, int maxDegreeFlag, int maxAbsDegree
+  ) {
+    // 90°
+    if (maxDegreeFlag == MAX_DEG_90) {
+      qrSigns = {-1, -1, 1, 1};
+      qrAdjustments = {maxAbsDegree, 0, -maxAbsDegree, 0};
+    }
+    else { // 180°
+      qrSigns = {1, 1};
+      qrAdjustments = {-maxAbsDegree, 0};
+    }
+
+    // inverse for negative
+    if (valSign == -1) {
+      for (int i = 0; i < qrSigns.size(); i++) {
+        qrSigns[i] *= -1;
+        qrAdjustments[i] *= -1;
+      }
+    }
+  }
+
+  // ex: [ -7.123, 365.123 ] to [ -7.123, xxx.yyy ]
   void Converter::normalizeDecimalAngle(double &axis, int maxDegreeFlag) {
 
     int initIntAxis = int(axis),
@@ -52,6 +90,46 @@ namespace coordinate_tools {
     intAxis %= maxAbsDegree;
     double realAxis = qrAdjustments[qrIndex] + qrSigns[qrIndex] * (intAxis + fracAxis);
     axis = realAxis;
+  }
+
+  // ex: [ 7°7'22.80"S, 365'22.80"E ] to [ 7°7'22.80"S, xxx°xx'yy.yy"* ]
+  void Converter::normalizeDMSAngle(DMSAxis &axis, int maxDegreeFlag) {
+
+    int newAxis = std::abs(axis.getDeg()),
+        maxAbsDegree;
+
+    // normalize 'maxDegreeFlag'
+    setMaxAbsoluteDegree(maxAbsDegree, maxDegreeFlag);
+
+    // less degree don't need to process
+    if (newAxis <= maxAbsDegree) return;
+
+    // default for positive
+    std::vector<int> qrSigns, qrAdjustments;
+
+    // 'qr' for 'quadrant'
+    setQuadrantDeterminators(
+      qrSigns, qrAdjustments,
+      axis.getDeg() / newAxis, maxDegreeFlag, maxAbsDegree
+    );
+
+    // process with remainder
+    int qrIndex = (newAxis / maxAbsDegree - 1) % qrSigns.size();
+    newAxis %= maxAbsDegree;
+
+    // assign to new value
+    axis.setDeg(qrAdjustments[qrIndex] + qrSigns[qrIndex] * newAxis);
+
+    // fraction value
+    if (axis.getMin() > 0 || axis.getSec() > 0) {
+      axis.setDeg(axis.getDeg() - 1);
+
+      // version 2
+      double newFrac = 1 - axis.getMin() / 60 - axis.getSec() / 3600;
+      double newMin = newFrac * 60;
+      axis.setMin(int(newMin));
+      axis.setSec((newMin - int(newMin)) * 60);
+    }
   }
 
   void Converter::switchDecimalAxis(DecimalPoint &pt) {
