00f6695dd4ddea15402329da99d1193841949f79
bostonsinaga <bostonsinga@gmail.com>
2024-05-23 18:31:21 +0700

    Call parser string test methods and display with iterating detected points
    Improve output with empty coordiante message

--- c++/main.cpp
+++ c++/main.cpp
@@ -3,17 +3,31 @@
 
 using namespace coordinate_tools;
 
-int main(int argc, char *argv[]) {    
+int main(int argc, char *argv[]) {
 
     Parser parser;
     parser.reset();
+    bool anySucceed = false;
 
-    if (parser.testDecimal(argv[1])) {
-        for (auto &pt : parser.getAllPoints()) {
-            std::cout << "Lat: " << pt.lat << ", ";
-            std::cout << "Lng: " << pt.lng << std::endl;
+    if (parser.testDMS(std::string(argv[1]))) {
+        std::cout << "\nResult:\n\n";
+
+        for (auto &pt : parser.getAllDMSPoints()) {
+            anySucceed = true;
+            std::cout << pt.stringifyPair() << std::endl;
         }
     }
+    else if (parser.testDecimal(std::string(argv[1]))) {
+        std::cout << "\nResult:\n\n";
+
+        for (auto &pt : parser.getAllDecimalPoints()) {
+            anySucceed = true;
+            std::cout << pt.lat << ',' << pt.lng << std::endl;
+        }
+    }
+    
+    if (anySucceed) std::cout << "\nProcess end.\n";
+    else std::cout << "\nProcess end. No coordinate.\n";
 
     return 0;
 }
